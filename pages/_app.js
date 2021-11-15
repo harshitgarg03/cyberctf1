d3292f59708f203585c451814db28f5b85c08180
harshitgarg03 <gharshit0323@gmail.com>
2021-11-15 23:23:44 +0800

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+// React redux imports
+import store from "../redux/store/store";
+import { Provider } from "react-redux";
+import { createWrapper } from "next-redux-wrapper";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <Provider store={store}>
+        <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+//makeStore function that returns a new store for every request
+const makeStore = () => store;
+const wrapper = createWrapper(makeStore);
+
+//withRedux wrapper that passes the store to the App Component
+export default wrapper.withRedux(MyApp);
